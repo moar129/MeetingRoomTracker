a3104c4461fce11332b85192c2fa3c636a16de11
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-11-26 10:38:58 +0100

    lavet validering på model classes

--- MeetingRoomTrackerLib/Models/Room.cs
+++ MeetingRoomTrackerLib/Models/Room.cs
@@ -3,15 +3,38 @@ using MeetingRoomTrackerLib.EnumClasses;
 
 namespace MeetingRoomTrackerLib
 {
-    // TODO: Tilføj validering for Room-klassen, Så som en string må ikke være tom, Floor skal være positiv osv.
     public class Room
     {
+        private string _name;
+        private int _floor;
         public int Id { get; set; }
         public bool Status { get; set; }
         public RoomTypeEnum RoomType { get; set; }
-        public string Name { get; set; }
+        public string Name 
+        {
+            get { return _name; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                _name = value;
+            }
+        }
         public BuildingEnum Building { get; set; }
-        public int Floor { get; set; }
+        public int Floor 
+        {
+            get { return _floor; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Floor must be a non-negative integer.");
+                }
+                _floor = value;
+            } 
+        }
 
         public Room(int id, bool status, RoomTypeEnum rumType, string name, BuildingEnum bygning, int floor)
         {
--- MeetingRoomTrackerLib/Models/TimeLog.cs
+++ MeetingRoomTrackerLib/Models/TimeLog.cs
@@ -1,17 +1,45 @@
 ﻿
 namespace MeetingRoomTrackerLib.Models
 {
-    //TODO: Tilføj validering for TimeLog-klassen, Så som at startEvent skal være før endEvent osv.
     public class TimeLog
     {
-        // Overvej om vi skal bruge IntervalTimer, eller om det er overflødigt information.
+        private DateTime _startEvent;
+        private DateTime _endEvent;
         public int Id { get; set; }
         public int RoomId { get; set; }
-        public DateTime StartEvent { get; set; }
-        public DateTime EndEvent { get; set; }
+        public DateTime StartEvent 
+        { 
+            get { return _startEvent; }
+            set
+            {
+                if (value > EndEvent)
+                {
+                    throw new ArgumentException("StartEvent must be earlier than EndEvent.");
+                }
+                if (value.Year > 2024)
+                {
+                    throw new ArgumentException("StartEvent year cannot be greater than 2024.");
+                }
 
-        public DateTime IntervalTimer { get; set; }
-
+                _startEvent = value;
+            } 
+        }
+        public DateTime EndEvent 
+        {
+            get { return _endEvent; }
+            set
+            {
+                if (value < StartEvent)
+                {
+                    throw new ArgumentException("EndEvent must be later than StartEvent.");
+                }
+                if (value.Year > 2024)
+                {
+                    throw new ArgumentException("EndEvent year cannot be greater than 2024.");
+                }
+                _endEvent = value;
+            } 
+        }
         public virtual Room Room { get; set; } = null;
 
         public TimeLog(int id, int roomId, DateTime startEvent, DateTime endEvent, DateTime intervalt)
@@ -20,14 +48,13 @@ namespace MeetingRoomTrackerLib.Models
             RoomId = roomId;
             StartEvent = startEvent;
             EndEvent = endEvent;
-            IntervalTimer = intervalt;
         }
         public TimeLog()
         {}
 
         public override string ToString()
         {
-            return $"Id: {Id}, RoomId: {RoomId}, StartEvent: {StartEvent:dd.MM.yyyy HH:mm:ss}, EndEvent: {EndEvent:dd.MM.yyyy HH:mm:ss}, IntervalTimer: {IntervalTimer:dd.MM.yyyy HH:mm:ss}";
+            return $"Id: {Id}, RoomId: {RoomId}, StartEvent: {StartEvent:dd.MM.yyyy HH:mm:ss}, EndEvent: {EndEvent:dd.MM.yyyy HH:mm:ss}";
         }
 
     }
