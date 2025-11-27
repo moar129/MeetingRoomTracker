3204a537280fee49b9edbd6b7ad87e0cea48a742
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-11-28 00:09:01 +0100

    har gjort så vi kan bruge swagger på hjemmesiden

--- MeetingRoomTrackerApi/Program.cs
+++ MeetingRoomTrackerApi/Program.cs
@@ -43,9 +43,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
