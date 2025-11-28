a231316558fc96a9f41a8ab9fb30607b0724a8d8
Jens Hincke Friis <jef003@edu.zealand.dk>
2025-11-28 14:47:16 +0100

    Nu burde der være secret på connection string

--- MeetingRoomTrackerApi/Program.cs
+++ MeetingRoomTrackerApi/Program.cs
@@ -27,10 +27,8 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 // Database
-var connectionString = "Server=mssql3.unoeuro.com;Database=devnoter_dk_db_dev_noter;User Id=devnoter_dk;Password=dhcED6fzFnR3A94GyHxb;Encrypt=True;TrustServerCertificate=True;";
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//var connectionString = Environment.GetEnvironmentVariable("DefaultConnection"); // bruger online 
-//Console.WriteLine("Connection String: " + connectionString);
+//var connectionString = "Server=mssql3.unoeuro.com;Database=devnoter_dk_db_dev_noter;User Id=devnoter_dk;Password=dhcED6fzFnR3A94GyHxb;Encrypt=True;TrustServerCertificate=True;";
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
 builder.Services.AddDbContext<RMTDbContext>(options =>
     options.UseSqlServer(connectionString));
 
